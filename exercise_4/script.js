44033d4966224569a0d650353429f5e1a0f88e6d
biancamutaquiha <biafersil@gmail.com>
2022-09-23 19:32:40 +0200

    exercise 4 - DOM firsts steps

---
+++ exercise_4/script.js
@@ -0,0 +1,8 @@
+//Get Element By Id
+console.log(document.getElementById('blog-title'));
+
+//Get Elements By Class Name
+console.log(document.getElementsByClassName('one'));
+
+//Get Elements by Tag Name
+console.log(document.getElementsByTagName('meta'))
