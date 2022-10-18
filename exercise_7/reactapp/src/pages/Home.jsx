ead6b4791b15aee8b261b2a5d8a8288469efb68f
biancamutaquiha <biafersil@gmail.com>
2022-10-18 18:52:24 +0200

    knowing fragment

--- exercise_7/reactapp/src/pages/Home.jsx
+++ exercise_7/reactapp/src/pages/Home.jsx
@@ -1,6 +1,11 @@
 function Home() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <input type="text" placeholder="type here" />
+      <button>Send</button>
+    </>
+    
   )
 }
 
