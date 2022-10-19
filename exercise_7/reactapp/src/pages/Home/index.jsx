521d7b64505132aa62d75dabfbf695ffb2d0d969
biancamutaquiha <biafersil@gmail.com>
2022-10-19 16:57:05 +0200

    understanding use state

--- exercise_7/reactapp/src/pages/Home/index.jsx
+++ exercise_7/reactapp/src/pages/Home/index.jsx
@@ -1,16 +1,40 @@
+import React, {useState} from 'react';
 import './styles.css';
 import { Card } from '../../components/Card';
 
 function Home() {
+  const [personName, setPersonName] = useState();
+  const [people, setPeople] = useState([]);
+  
+  function handleAddStudant(){
+    const newPerson = {
+      name: personName,
+      time: new Date().toLocaleTimeString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+
+    setPeople(prevState => [...prevState, newPerson])
+  }
+  
   return (
     <div className='container'>
       <h1>Presence List</h1>
-      <input type="text" placeholder="type here" />
-      <button>Send</button>
+      <input 
+        type="text" 
+        placeholder="type here"
+        onChange={e => setPersonName(e.target.value)} 
+      />
+      <button type='button' onClick={handleAddStudant}>
+        Send
+      </button>
 
-      <Card name="Bianca" time="12:30"/>
-      <Card name="Olivio" time="14:23"/>
-      <Card name="Jeppe" time="16:45"/>
+      {
+        people.map(person => <Card name={person.name} time={person.time}/>)
+      }
+      
     </div>
     
   )
